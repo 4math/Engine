28313f9cdf37e687416aab190427f586daf96e76
Kirill Trofimov <kirilltrofimov@inbox.lv>
2019-03-18 23:02:35 +0200

    Added m_game_name to GenericGame class

--- Engine/src/GenericGame.h
+++ Engine/src/GenericGame.h
@@ -1,21 +1,27 @@
 #pragma once
 
+#include <string>
+
 constexpr double DEFAULT_GAME_TICKRATE = 60.0; // 60 game updates per second
 
 class GenericGame
 {
 	// VARIABLES
-private:
+protected:
 	double m_game_tickrate = DEFAULT_GAME_TICKRATE; // Game updates per second
-
+	std::string m_game_name;
+	
+	
 	// CONSTRUCTORS/DESTRUCTORS
 public:
 	GenericGame() {};
 	virtual ~GenericGame() {};
 
+	
 	// METHODES
 public:
 	virtual void FrameAction() = 0; // This method will be called every game tick
 
 	double& GameTickrate() { return m_game_tickrate; }
+	std::string& GameName() { return m_game_name; }
 };
