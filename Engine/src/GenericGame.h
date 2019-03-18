814d74d847ade31334023108de637d12b71930e8
Kirill Trofimov <kirilltrofimov@inbox.lv>
2019-03-18 21:28:42 +0200

    Enhanced GenericGame class code

--- Engine/src/GenericGame.h
+++ Engine/src/GenericGame.h
@@ -1,15 +1,17 @@
 #pragma once
 
+constexpr double DEFAULT_GAME_TICKRATE = 60.0; // 60 game updates per second
+
 class GenericGame
 {
 	// VARIABLES
 private:
-	double m_fps_limit = 60.0;
+	double m_game_tickrate = DEFAULT_GAME_TICKRATE; // Game updates per second
 
 	// CONSTRUCTORS/DESTRUCTORS
 public:
 	GenericGame() { Initialize(); };
-	~GenericGame() { Shutdown(); };
+	virtual ~GenericGame() { Shutdown(); }; // Is virtual destructor needed here?
 
 	// METHODES
 private:
@@ -18,4 +20,6 @@ private:
 
 public:
 	virtual void FrameAction() = 0; // This method will be called every game tick
+
+	double& GameTickrate() { return m_game_tickrate; }
 };
