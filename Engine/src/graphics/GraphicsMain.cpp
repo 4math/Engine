a55cbba78d5dc7934fe234972549737aa19b1f36
SecyrB <oleggedzjuns@gmail.com>
2019-04-02 14:56:08 +0300

    Added validation layers creation Added debug messenger creation and dellition

--- Engine/src/graphics/GraphicsMain.cpp
+++ Engine/src/graphics/GraphicsMain.cpp
@@ -11,15 +11,23 @@ void graphics::GraphicsManager::Initialize()
 void graphics::GraphicsManager::Shutdown()
 {
 	vkDestroyDevice(m_vk_device, nullptr);
+	if (m_enable_validation_layers)
+	{
+		DestroyDebugUtilsMessengerEXT(nullptr);
+	}
 	vkDestroyInstance(m_vk_instance, nullptr);
 	m_initialized = false;
 }
 
 bool graphics::GraphicsManager::InitializeVulkan()
 {
+#ifdef _DEBUG
+	m_enable_validation_layers = true;
+#endif
 	try
 	{
 		CreateInstance();
+		SetupDebugMessenger();
 		PickPhysicalDevice();
 		CreateLogicalDevice();
 	}
@@ -27,15 +35,19 @@ bool graphics::GraphicsManager::InitializeVulkan()
 	{
 #ifdef _DEBUG
 		std::cerr << e.what() << "\n";
-#endif // _DEBUG
+#endif
 		return false;
 	}
-
 	return true;
 }
 
 void graphics::GraphicsManager::CreateInstance()
 {
+	if (m_enable_validation_layers && !CheckValidationLayerSupport())
+	{
+		throw std::runtime_error("validation layers requested, but not available!");
+	}
+
 	VkApplicationInfo app_info = {};
 	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	app_info.pApplicationName = m_app_name.c_str();
@@ -44,39 +56,47 @@ void graphics::GraphicsManager::CreateInstance()
 	app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 	app_info.apiVersion = VK_API_VERSION_1_0;
 
-	uint32_t extension_count = 0;
-	auto required_extensions = glfwGetRequiredInstanceExtensions(&extension_count);
-	auto present_extensions = ListInstanceExtensions(false);
-
-	// Checking if required extensions are available
-	for (unsigned extension_id = 0; extension_id < extension_count; extension_id++)
-	{
-		bool status = false;
-		std::string required_extension_name = required_extensions[extension_id];
-		for (const auto present_extension : present_extensions)
-		{
-			std::string present_extension_name = present_extension.extensionName;
-			if (required_extension_name == present_extension_name)
-			{
-				status = true;
-				break;
-			}
-		}
-
-		if (status != true)
-			throw std::runtime_error("Required extension " + required_extension_name + " was not found");
-	}
-
 	VkInstanceCreateInfo create_info = {};
 	create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	create_info.pApplicationInfo = &app_info;
-	create_info.enabledExtensionCount = extension_count;
-	create_info.ppEnabledExtensionNames = required_extensions;
-	create_info.enabledLayerCount = 0;
+
+	auto extensions = GetRequiredExtensions();
+	create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	create_info.ppEnabledExtensionNames = extensions.data();
+
+	if (m_enable_validation_layers) {
+		create_info.enabledLayerCount = static_cast<uint32_t>(m_validation_layers.size());
+		create_info.ppEnabledLayerNames = m_validation_layers.data();
+	}
+	else {
+		create_info.enabledLayerCount = 0;
+	}
 
 	VkResult result = vkCreateInstance(&create_info, nullptr, &m_vk_instance);
 	if (result != VK_SUCCESS)
 		throw std::runtime_error("Failed to create VkInstance, error code: " + std::to_string(result));
+}
+
+void graphics::GraphicsManager::SetupDebugMessenger()
+{
+	if (!m_enable_validation_layers) return;
+
+	VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	createInfo.messageSeverity = 
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | 
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	// Add VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT for full info
+	createInfo.messageType = 
+		VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | 
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | 
+		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	createInfo.pfnUserCallback = DebugCallback;
+
+	if (CreateDebugUtilsMessengerEXT(&createInfo, nullptr) != VK_SUCCESS) {
+		throw std::runtime_error("failed to set up debug messenger!");
+	}
 }
 
 void graphics::GraphicsManager::PickPhysicalDevice()
@@ -125,6 +145,70 @@ void graphics::GraphicsManager::CreateLogicalDevice()
 	vkGetDeviceQueue(m_vk_device, indices.graphics_family.value(), 0, &m_vk_graphics_queue);
 }
 
+bool graphics::GraphicsManager::CheckValidationLayerSupport()
+{
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	for (const char* layerName : m_validation_layers) 
+	{
+		bool layerFound = false;
+		for (const auto& layerProperties : availableLayers) 
+		{
+			if (strcmp(layerName, layerProperties.layerName) == 0) 
+			{
+				layerFound = true;
+				break;
+			}
+		}
+		if (!layerFound) 
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+std::vector<const char*> graphics::GraphicsManager::GetRequiredExtensions()
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+	if (m_enable_validation_layers) 
+	{
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+	return extensions;
+}
+
+VkResult graphics::GraphicsManager::CreateDebugUtilsMessengerEXT(
+	const VkDebugUtilsMessengerCreateInfoEXT* create_info_,
+	const VkAllocationCallbacks* allocator_)
+{
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(m_vk_instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr) 
+	{
+		return func(m_vk_instance, create_info_, allocator_, &m_vk_debug_messenger);
+	}
+	else
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void graphics::GraphicsManager::DestroyDebugUtilsMessengerEXT(const VkAllocationCallbacks* allocator_)
+{
+	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(m_vk_instance, "vkDestroyDebugUtilsMessengerEXT");
+	if (func != nullptr) 
+	{
+		func(m_vk_instance, m_vk_debug_messenger, allocator_);
+	}
+}
+
 void graphics::GraphicsManager::BeginFrame()
 {
 
@@ -133,4 +217,14 @@ void graphics::GraphicsManager::BeginFrame()
 void graphics::GraphicsManager::EndFrame()
 {
 
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL graphics::DebugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT message_severity_,
+	VkDebugUtilsMessageTypeFlagsEXT message_type_,
+	const VkDebugUtilsMessengerCallbackDataEXT* callback_data_,
+	void* user_data_)
+{
+	std::cerr << "Validation layer : " << callback_data_->pMessage << std::endl;
+	return VK_FALSE;
 }
