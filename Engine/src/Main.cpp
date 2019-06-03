8d3bd63ec484585b984a5784ff6e462d0f0c3b06
SecyrB <oleggedzjuns@gmail.com>
2019-06-03 16:14:08 +0300

    Changed debug game deletion for debugging purposes

--- Engine/src/Main.cpp
+++ Engine/src/Main.cpp
@@ -19,6 +19,15 @@ public:
 
 int main(int argc, char** argv)
 {
+	
+#ifdef _DEBUG
+	Game* debug_game = new Game;
+	int return_value = debug_game->Loop();
+	delete debug_game;
+	std::cin.get();
+	return return_value;
+#else
 	Game debug_game;
 	return debug_game.Loop();
+#endif
 }
